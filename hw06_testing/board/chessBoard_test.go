67659d57f939b683ee9b292d9bbf21e2a6f47734
AlexSheg91 <138349777+newBird57@users.noreply.github.com>
2023-09-17 22:36:44 +0900

    board_test

---
+++ hw06_testing/board/chessBoard_test.go
@@ -0,0 +1,27 @@
+package board_test
+
+import (
+	"fmt"
+	"testing"
+
+	board "github.com/AlexSH61/homework_basic/hw06_testing/board"
+)
+
+func TestBoardSiz(t *testing.T) {
+	tests := []struct {
+		size int
+		want string
+	}{
+		{size: 1, want: "# \n"},
+		{size: 3, want: "#  # \n #  \n#  # \n"},
+		{size: 4, want: "#  #  \n #  # \n#  #  \n #  # \n"},
+	}
+	for _, size := range tests {
+		t.Run(fmt.Sprintf("size%d", size.size), func(t *testing.T) {
+			result := board.Boardsize(size.size)
+			if result != size.want {
+				t.Errorf("size %d: %q %q", size.size, size.want, result)
+			}
+		})
+	}
+}
