4daeda04d3cb17b9c2437fd489795bb5ec9fae16
AlexSheg91 <138349777+newBird57@users.noreply.github.com>
2023-09-30 23:32:33 +0900

    final

--- hw06_testing/hw04_testing/type_book/book_test.go
+++ hw06_testing/hw04_testing/type_book/book_test.go
@@ -3,9 +3,8 @@ package book_test
 import (
 	"testing"
 
-	"github.com/stretchr/testify/assert"
-
 	book "github.com/AlexSH61/homework_basic/hw06_testing/hw04_testing/type_book"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestBookType(t *testing.T) {
