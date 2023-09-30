9168404212681b1761a9b6d7f01ee098e31cc84e
AlexSheg91 <138349777+newBird57@users.noreply.github.com>
2023-09-30 23:42:58 +0900

    final2

--- hw06_testing/hw04_testing/type_book/book_test.go
+++ hw06_testing/hw04_testing/type_book/book_test.go
@@ -12,14 +12,19 @@ func TestBookType(t *testing.T) {
 
 	book.ID(3)
 	assert.Equal(t, 3, book.GetID())
+
 	book.Title("Master and Margarita")
 	assert.Equal(t, "Master and Margarita", book.GetTitle())
+
 	book.Author("Michail Bulgakov")
 	assert.Equal(t, "Michail Bulgakov", book.GetAuthor())
+
 	book.Year(1867)
 	assert.Equal(t, 1867, book.GetYear())
+
 	book.Size(500)
 	assert.Equal(t, 500, book.GetSize())
+
 	book.Rating(4.5)
 	assert.Equal(t, float64(4.5), book.GetRating())
 }
