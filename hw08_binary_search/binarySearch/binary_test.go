3240ec7df66f36d7cce0f69b58a73cd65a9f2fd5
AlexSheg91 <138349777+newBird57@users.noreply.github.com>
2023-10-15 21:56:09 +0900

    hw08_binary_search

---
+++ hw08_binary_search/binarySearch/binary_test.go
@@ -0,0 +1,37 @@
+package binary_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	binary "github.com/AlexSH61/homework_basic/hw08_binary_search/binarySearch"
+)
+
+func TestBinary(t *testing.T) {
+	testCase := []struct {
+		name         string
+		inputArray   []int
+		intputNumber int
+		expected     int
+	}{
+		{
+			name:         "sample",
+			inputArray:   []int{2, 3, 4, 5, 6, 7, 8, 89},
+			intputNumber: 5,
+			expected:     3,
+		},
+		{
+			name:         "sample_unfounded",
+			inputArray:   []int{2, 3, 4, 5, 6, 7, 8, 89},
+			intputNumber: 9,
+			expected:     -1,
+		},
+	}
+	for _, tc := range testCase {
+		t.Run(tc.name, func(t *testing.T) {
+			result := binary.Searching(tc.intputNumber, tc.inputArray)
+			assert.Equal(t, tc.expected, result)
+		})
+	}
+}
