f69b1766203068ac6e64143d4a39bf43a416e573
AlexSheg91 <138349777+newBird57@users.noreply.github.com>
2023-10-16 22:11:38 +0900

    hw08_binary_search

--- hw08_binary_search/binarySearch/binary.go
+++ hw08_binary_search/binarySearch/binary.go
@@ -2,15 +2,17 @@ package binary
 
 func Searching(needle int, array []int) int {
 	low, high := 0, len(array)-1
-	for low <= high {
-		med := (low + high) / 2
-		switch array[med] == needle {
-		case array[med] < needle:
-			low = med + 1
-		case array[med] > needle:
-			high = med - 1
-		default:
-			return med
+	if needle >= low && needle <= high {
+		for low <= high {
+			med := (low + high) / 2
+			switch array[med] == needle {
+			case array[med] < needle:
+				low = med + 1
+			case array[med] > needle:
+				high = med - 1
+			default:
+				return med
+			}
 		}
 	}
 	return -1
