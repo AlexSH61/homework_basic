60c571cef340eb0703385ee6853b936c5964ecbb
AlexSheg91 <138349777+newBird57@users.noreply.github.com>
2023-12-12 04:31:14 +0900

    fix4

--- hw10_motion_sensor/process_data/process_test.go
+++ hw10_motion_sensor/process_data/process_test.go
@@ -10,7 +10,6 @@ func TestProcessDataRoutine(t *testing.T) {
 	testData := []float64{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20}
 	dataChannel := make(chan float64, len(testData))
 	processedDataChannel := make(chan float64)
-
 	go ProcessDataRoutine(dataChannel, processedDataChannel)
 
 	for _, data := range testData {
