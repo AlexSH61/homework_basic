aa690742aeeeb99e38067293297800bcc3e9564c
AlexSheg91 <138349777+newBird57@users.noreply.github.com>
2023-12-28 02:00:57 +0900

    first

--- hw11_worker_pool/main.go
+++ hw11_worker_pool/main.go
@@ -1,5 +1,26 @@
 package main
 
+import (
+	"fmt"
+	"sync"
+)
+
+var counter int
+var mutex sync.Mutex
+
+func Worker(id int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	mutex.Lock()
+	counter++
+	mutex.Unlock()
+	fmt.Printf("worker completed: %d\n", id)
+}
 func main() {
-	// Place your code here.
+	var wg sync.WaitGroup
+	for i := 1; i <= 5; i++ {
+		wg.Add(1)
+		go Worker(i, &wg)
+	}
+	wg.Wait()
+	fmt.Printf("final counter : %d\n", counter)
 }
